421d1b80915c76d9d48cb3e9c76d741148cdc78a
ViktorBizbiz <bizbizgennadij@gmail.com>
2022-10-10 14:38:58 +0300

    first version of CRUD-app

---
+++ src/main/java/ua/bizbiz/springpractise/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package ua.bizbiz.springpractise.dao;
+
+import org.springframework.stereotype.Component;
+import ua.bizbiz.springpractise.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Elliot"));
+        people.add(new Person(++PEOPLE_COUNT, "Darlene"));
+        people.add(new Person(++PEOPLE_COUNT, "Angela"));
+        people.add(new Person(++PEOPLE_COUNT, "Mr.Robot"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
