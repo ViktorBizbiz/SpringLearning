b5225a0d7d7e4783eb93d7aca2c7072bfad8fa1b
ViktorBizbiz <bizbizgennadij@gmail.com>
2022-11-27 22:23:15 +0200

    Validation of Forms"'s lesson (Unstable)

--- src/main/java/ua/bizbiz/springpractise/models/Person.java
+++ src/main/java/ua/bizbiz/springpractise/models/Person.java
@@ -1,9 +1,19 @@
 package ua.bizbiz.springpractise.models;
 
+
+import javax.validation.constraints.*;
+
 public class Person {
     private int id;
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
+
+    @Min(value = 0, message = "Age should be greater than 0")
     private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
     private String email;
 
     public Person() {
